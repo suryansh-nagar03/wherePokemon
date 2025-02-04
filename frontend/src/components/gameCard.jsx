ade495dbde19886ce7a04a42679a74e06e758f6f
Suryansh Nagar <nagarsuryansh72@gmail.com>
2025-02-05 02:40:54 +0530

    Completed home page

--- frontend/src/components/gameCard.jsx
+++ frontend/src/components/gameCard.jsx
@@ -1,10 +1,10 @@
-export default function GameCard({url}){
+export default function GameCard({url,text}){
     return (
         <>
-            <div className= "flex flex-col justify-center items-center p-2 gap-2 bg-white shadow-lg rounded-lg hover:scale-98 hover:cursor-pointer">
+            <div className= "flex flex-col justify-center items-center p-2 gap-2 bg-white shadow-lg rounded-lg ">
                 <img src={url} alt="" className="h-60 w-60"/>
-                <div className="tracking-tight">First Generation</div>
-                <button className="bg-blue-500 text-white p-2 pt-1 pb-1 rounded-lg">
+                <div className="tracking-tight">{text}</div>
+                <button className="bg-[#6929b1] text-white p-2 pt-1 pb-1 rounded-lg hover:scale-95 hover:cursor-pointer hover:opacity-80">
                     <div>Start</div>
                 </button>
             </div>
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom"
 function Home(){
     return (
         <>
-            <div className="flex justify-center gap-10 pt-10">
-                <Link to={"leaderboard"}><div>LeaderBoard</div></Link>
-                <a href="">Git Repo</a>
+            <div className="flex justify-center gap-4 pt-10 tracking-tighter">
+                <Link to={"leaderboard"}><div className="bg-white rounded-md p-1 font-bold">Leaderboard</div></Link>
+                <a href="https://github.com/suryansh-nagar03/wherePokemon" className="bg-white rounded-md p-1 font-bold">Git Repo</a>
             </div>
             <div className="flex justify-center gap-10 pt-10">
-                <GameCard url="/background.jpg"></GameCard>
-                <GameCard url="/background.jpg"></GameCard>
-                <GameCard url="/background.jpg"></GameCard>
+                <GameCard url="/first-game.jpg" text = {"First Generation"}></GameCard>
+                <GameCard url="/second-game.jpg" text = {"Second Generation"}></GameCard>
+                <GameCard url="/third-game.png" text = {"Third Generation"}></GameCard>
             </div>
         </>
     )
